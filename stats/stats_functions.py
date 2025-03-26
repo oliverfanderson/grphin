8ebc0a3dec7778002fb82ec523e92d69842be79c
ctrlaltaf <altafayyubibarelvi@gmail.com>
2025-03-26 12:27:33 -0700

    adding stress protein analysis

--- stats/stats_functions.py
+++ stats/stats_functions.py
@@ -1,13 +1,19 @@
 import ast
 import csv
 import os
+from pathlib import Path
+import random
 import re
 import sys
 from matplotlib import pyplot as plt
 import numpy as np
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from refactor import load_graphlet_config, initialize_three_node_graphlet_data
+from refactor import (
+    initialize_graphlet_data,
+    load_graphlet_config,
+    initialize_three_node_graphlet_data,
+)
 
 
 def plot_runtime_stats():
@@ -32,7 +38,11 @@ def plot_runtime_stats():
 
 
 def plot_three_node_graphlet_distribution(
-    three_node_graphlet_count, three_node_graphlet_namespace, three_node_graphlet_id, species, output_dir
+    three_node_graphlet_count,
+    three_node_graphlet_namespace,
+    three_node_graphlet_id,
+    species,
+    output_dir,
 ):
     print("plotting graphlet distribution")
     hist_data = []
@@ -50,11 +60,11 @@ def plot_three_node_graphlet_distribution(
     plt.title(f"{species} Graphlet Count Distribution", fontsize=16)
     plt.xlabel("Graphlet Index", fontsize=14)
     plt.ylabel("Count (log scale)", fontsize=14)
-    plt.xticks(x_label[::2], fontsize=8)
+    plt.xticks(x_label[::2], fontsize=10)
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.tight_layout()
     plt.savefig(f"{output_dir}/three_node_graphlet_dist.pdf")
-    plt.show()
+    # plt.show()
 
     sorted_graphlet_dict = {
         key: value
@@ -73,7 +83,7 @@ def plot_three_node_graphlet_distribution(
     return None
 
 
-def read_output_files(output_dir):
+def read_output_files(output_dir, species):
     three_node_graphlet_count = {}
     three_node_graphlet_namespace = {}
     three_node_orbit_protein_data = {}
@@ -83,9 +93,6 @@ def read_output_files(output_dir):
     run_time = 0
 
     graphlet_config = load_graphlet_config("graphlet_config.csv")
-
-    # for graphlet in graphlet_config:
-    #     print(graphlet)
 
     # using the graphlet_config to initialize dictionaries with information
     (
@@ -114,9 +121,6 @@ def read_output_files(output_dir):
             three_node_graphlet_namespace[graphlet_hash] = graphlet
     print("post processing step 1/6")
 
-    # for key in three_node_graphlet_namespace:
-    #     print(key, three_node_graphlet_namespace[key])
-
     with open(f"{output_dir}/graphlet_counts.csv", "r") as file:
         csv_reader = csv.reader(file, delimiter=":")
         for row in csv_reader:
@@ -125,18 +129,12 @@ def read_output_files(output_dir):
             three_node_graphlet_count[hash(graphlet)] = count
     print("post processing step 2/6")
 
-    # for key in three_node_graphlet_count:
-    #     print(key, three_node_graphlet_count[key])
-
     with open(f"{output_dir}/orbit_hash_mapper.csv", "r") as file:
         csv_reader = csv.reader(file, delimiter=":")
         for row in csv_reader:
             orbit_hash = int(row[0])
             three_node_orbit_namespace[orbit_hash] = row[1]
     print("post processing step 3/6")
-
-    # for key in three_node_orbit_namespace:
-    #     print(key, three_node_orbit_namespace[key])
 
     with open(f"{output_dir}/graphlet_id_mapper.csv", "r") as file:
         csv_reader = csv.reader(file, delimiter=":")
@@ -145,9 +143,6 @@ def read_output_files(output_dir):
             id = row[1].strip()
             three_node_graphlet_id[hash(graphlet)] = id
     print("post processing step 4/6")
-
-    # for key in three_node_graphlet_id:
-    #     print(key, three_node_graphlet_id[key])
 
     orbit_id_dict = {}
     with open(f"{output_dir}/orbit_id_mapper.csv", "r") as file:
@@ -160,12 +155,6 @@ def read_output_files(output_dir):
                 three_node_orbit_id[hash(result)] = id
                 orbit_id_dict[id] = result
     print("post processing step 5/6")
-
-    # for key in three_node_orbit_id:
-    #     print(key, three_node_orbit_id[key])
-
-    # for key in orbit_id_dict:
-    #     print(key, orbit_id_dict[key])
 
     node_orbit_arr = np.loadtxt(
         f"{output_dir}/node_orbit.csv", delimiter=",", dtype=int
@@ -183,8 +172,10 @@ def read_output_files(output_dir):
             print(count, "/", rows * cols, end="\r")
             count += 1
 
-    # for key in three_node_orbit_protein_data:
-    #     print(key, three_node_orbit_protein_data[key][1])
+    compare_files(
+        Path(f"output_refactor/{species}/node_orbit.csv"),
+        Path(f"final_output/{species}/node_orbit.csv"),
+    )
 
     return (
         three_node_graphlet_count,
@@ -194,13 +185,158 @@ def read_output_files(output_dir):
         three_node_graphlet_id,
         three_node_orbit_id,
         graphlet_config,
+        node_orbit_arr,
     )
+
+
+def get_stress_proteins(protein_id_dict, stress_path, delimiter):
+    stress_proteins = []
+
+    with open(stress_path, "r") as file:
+        next(file)
+        reader = csv.reader(file, delimiter=delimiter)
+        for line in reader:
+            if line[0] in protein_id_dict:
+                stress_proteins.append(protein_id_dict[line[0]])
+    return stress_proteins
+
+
+def plot_stress_orbit_distribution(
+    three_node_orbit_protein_data,
+    three_node_orbit_id,
+    stress_proteins_list,
+    protein_id,
+    species,
+    output_dir,
+    node_orbit_arr,
+):
+
+    observed_median_count = {}
+    # print("Stress proteins : ", stress_proteins_list)
+    stress_protein_orbit_dict = {}
+    print("Counting stress proteins\n")
+    for orbit in three_node_orbit_protein_data:
+        # print(f"orbit {orbit}", end="\r")
+        stress_protein_orbit_dict[orbit] = []
+        for protein in stress_proteins_list:
+            protein_orbit_count = node_orbit_arr[protein][
+                int(three_node_orbit_id[orbit])
+            ]
+            stress_protein_orbit_dict[orbit] += [protein_orbit_count]
+
+    print("Counting stress medians\n")
+    for orbit in stress_protein_orbit_dict:
+        # print(f"orbit {orbit}", end="\r")
+        orbit_list = []
+        if len(stress_protein_orbit_dict[orbit]) == 0:
+            orbit_list = [0]
+        else:
+            orbit_list = stress_protein_orbit_dict[orbit]
+        sorted_list = np.sort(orbit_list)
+        median = np.median(sorted_list)
+        observed_median_count[orbit] = median
+
+    sample = 1000
+    sample_size = len(stress_proteins_list)
+    print("getting non stress proteins \n")
+    non_stress_proteins = []
+    for protein in protein_id:
+        # print(f"protein {protein}", end="\r")
+        if protein_id[protein] not in stress_proteins_list:
+            non_stress_proteins.append(protein_id[protein])
+
+    sample_results = {}
+
+    for i in range(0, sample):
+        print("sample :", i, end="\r")
+        non_stress_sample = random.sample(non_stress_proteins, sample_size)
+        array_stack = []
+        for protein in non_stress_sample:
+            array_stack.append(node_orbit_arr[protein])
+        stacked = np.vstack(array_stack)
+        orbit_medians_list = np.median(stacked, axis=0)
+
+        for orbit in three_node_orbit_protein_data:
+            orbit_index = three_node_orbit_id[orbit]
+            if orbit not in sample_results:
+                sample_results[orbit] = []
+            sample_results[orbit] += [orbit_medians_list[int(orbit_index)]]
+
+    significance = {}
+
+    # Calculate if an orbit is significant
+    for orbit in three_node_orbit_protein_data:
+        count = 0
+        # from the vector of all the random medians at a given orbit
+        for random_median in sample_results[orbit]:
+            if observed_median_count[orbit] > random_median:
+                count += 1
+
+        if count >= float(sample) * 0.99:
+            significance[orbit] = 1
+            print(three_node_orbit_id[orbit], ": significant")
+        else:
+            significance[orbit] = 0
+            # print(three_node_orbit_id[orbit], ": NOT significant")
+
+    with open(f"{output_dir}/stress_orbit_significance.csv", "w") as f:
+        f.write(f"orbit_id\tsignificant?\tobserved_median\tvector_random_medians\n")
+        for orbit in significance:
+            f.write(
+                f"{three_node_orbit_id[orbit]}\t{significance[orbit]}\t{observed_median_count[orbit]}\t{sample_results[orbit]}\n"
+            )
+
+    i = 0
+    for orbit in three_node_orbit_protein_data:
+        if significance[orbit] == 1:
+            hist_data = sample_results[orbit]
+
+            fig = plt.figure(figsize=(14, 6))
+            plt.hist(hist_data)
+            plt.axvline(x=int(observed_median_count[orbit]), color="r", linestyle="--")
+            plt.title(
+                f"{species} Random Median Samples at Orbit {int(three_node_orbit_id[orbit])} Distribution",
+                fontsize=16,
+            )
+            plt.savefig(f"{output_dir}/sig_orbit/orbit{i}.pdf")
+            plt.close()
+        i += 1
+    return None
+
+
+def compare_files(file1, file2):
+    with open(file1, "rb") as f1, open(file2, "rb") as f2:
+        content1 = f1.read()
+        content2 = f2.read()
+        if content1 == content2:
+            print("node orbit files are the same")
+        else:
+            print("DIFFERENT node orbit files")
+        return content1 == content2
 
 
 def main():
     print("running stats")
-    output_dir = "output_refactor/bsub"
-    species = "bsub"
+
+    species = "cerevisiae"
+    output_dir = f"output_refactor/{species}"
+    input_ppi = f"data/{species}_ppi.csv"
+    input_reg = f"data/{species}_reg.csv"
+
+    if species == "bsub":
+        stress_dir = "data/oxidative_stress/txid224308/txid224308-stress-proteins.csv"
+    if species == "drerio":
+        stress_dir = "data/oxidative_stress/txid7955/txid7955-stress-proteins.csv"
+    if species == "fly":
+        stress_dir = "data/oxidative_stress/txid7227/txid7227-stress-proteins.csv"
+    if species == "elegans":
+        stress_dir = "data/oxidative_stress/txid6239/txid6239-stress-proteins.csv"
+    if species == "cerevisiae":
+        stress_dir = "data/oxidative_stress/txid559292/txid559292-stress-proteins.csv"
+
+    protein_id, G, G_prime, graphlet_config = initialize_graphlet_data(
+        input_ppi, input_reg
+    )
 
     (
         three_node_graphlet_count,
@@ -210,11 +346,34 @@ def main():
         three_node_graphlet_id,
         three_node_orbit_id,
         graphlet_config,
-    ) = read_output_files(output_dir)
+        node_orbit_arr,
+    ) = read_output_files(output_dir, species)
 
-    plot_three_node_graphlet_distribution(three_node_graphlet_count, three_node_graphlet_namespace, three_node_graphlet_id, species, output_dir)
+    plot_three_node_graphlet_distribution(
+        three_node_graphlet_count,
+        three_node_graphlet_namespace,
+        three_node_graphlet_id,
+        species,
+        output_dir,
+    )
 
-    
+    # significance orbit stats
+
+    stress_proteins_list = get_stress_proteins(protein_id, stress_dir, "\t")
+
+    significance = plot_stress_orbit_distribution(
+        three_node_orbit_protein_data,
+        three_node_orbit_id,
+        stress_proteins_list,
+        protein_id,
+        species,
+        output_dir,
+        node_orbit_arr,
+    )
+
+    # species wide stats
+
+    # two node graphlet stats
 
 
 if __name__ == "__main__":
